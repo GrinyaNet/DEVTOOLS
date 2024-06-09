d4ea8ff1609d42ba4e39e4e8bcdc0b2526179fc0
GrinyaNet <igrinya@yahoo.com>
2024-06-09 09:12:26 +0300

    Task2

--- Lesson7/Task2/webpack.config.js
+++ Lesson7/Task2/webpack.config.js
@@ -1,7 +1,7 @@
-const { webpack } = require("webpack");
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './src/index.js',
@@ -10,6 +10,10 @@ module.exports = {
     },
     module: {
         rules: [
+            {
+                test: /.js$/,
+                use: ['babel-loader']
+            },
             {
                 test: /.s?css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
@@ -25,11 +29,13 @@ module.exports = {
                             outputPath: 'images',
                         },
                     },
-                    ],
+                ],
             }
         ],
     },
     plugins: [
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: './src/index.html'
         }),
